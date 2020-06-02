60f7cd9dc9ff6c229993aef7ee77b1407c607b08
Shahid <shahid.sarker@gmail.com>
2020-06-02 14:06:53 -0400

    Extract Campuses and CampusCard components

---
+++ src/components/Campuses.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import CampusCard from "./CampusCard";
+
+class Campuses extends Component {
+  render() {
+    return (
+      <div className="campuses">
+        <CampusCard
+          name="Brooklyn College"
+          imageUrl="https://via.placeholder.com/150"
+          students={32}
+        />
+        <CampusCard
+          name="College of Staten Island"
+          imageUrl="https://via.placeholder.com/150"
+          students={26}
+        />
+        <CampusCard
+          name="John Jay"
+          imageUrl="https://via.placeholder.com/150"
+          students={25}
+        />
+      </div>
+    );
+  }
+}
+
+export default Campuses;
